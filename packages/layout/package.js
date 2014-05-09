e154b23a64d1869bcf23f91ea1c82cc641b752f7
Adithep <badsxx@gmail.com>
2014-05-09 19:38:00 +0700

    nginx

--- packages/layout/package.js
+++ packages/layout/package.js
@@ -13,13 +13,9 @@ Package.on_use(function (api, where, asset) {
     , 'alpha-stylus'
     , 'standard-app-packages']);
   api.add_files([
-    'liga/websymbolsligaregular.eot'
-    , 'liga/websymbolsligaregular.svg'
-    , 'liga/websymbolsligaregular.ttf'
-    , 'liga/websymbolsligaregular.woff'], 'client', {isAsset: true});
-  api.add_files([
     'layout.jade'
     , 'layout.coffee'
+    , 'essential.styl'
     , 'layout.styl'], 'client');
 });
 
